70c653498b801b72d5f957941e492493fd0ed3ee
ImMars0 <ioana666@gmail.com>
2025-08-18 09:32:02 +0300

    fixed API for url

--- src/API/urlApi.tsx
+++ src/API/urlApi.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import type { AxiosInstance } from "axios";
 
-const BASE_URL = "https://localhost:6969/api";
+const BASE_URL = "http://localhost:6969/api";
 
 export const apiClient: AxiosInstance = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, // Include cookies in requests
+  withCredentials: true,
 });
