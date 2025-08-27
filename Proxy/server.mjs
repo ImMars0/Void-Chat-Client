e74cb920a9a84ec4f6d94ce97243dc2d22f92dfc
ImMars0 <ioana666@gmail.com>
2025-08-27 14:14:32 +0300

    other

--- Proxy/server.mjs
+++ Proxy/server.mjs
@@ -9,12 +9,13 @@ app.use('/api', createProxyMiddleware({
   secure: false, 
 }));
 
-app.use('/chatHub', createProxyMiddleware({
+/*app.use('/chatHub', createProxyMiddleware({
   target: 'https://localhost:7124',
   ws: true,              
   changeOrigin: true,
   secure: false,
-}));
+  logLevel: 'debug',
+}));*/
 
 app.listen(6969, () => {
   console.log('Proxy server is running on http://localhost:6969')
